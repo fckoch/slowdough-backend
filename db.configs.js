8d14bafd466c1365816d87ce0f1559cc8dbb1a15
Filipe <filipe.koch@mouts.info>
2021-08-04 22:17:35 -0300

    add db params

---
+++ db.configs.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "bf7c17ec3a91de",
+    PASSWORD: "90ea298a",
+    DB: "heroku_fd08a1617dbbd6b"
+  };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://slowdough-api.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
